6dcfacb5c76305bb227674eac6d926e54a26f45c
Samuel Hoffman <samuelhoffman2@gmail.com>
2017-07-05 16:32:41 -0500

    Write Pixmap to a StringIO for uploading.

--- utils.py
+++ utils.py
@@ -1,5 +1,23 @@
 import platform
+import io
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import * 
+from PyQt5.QtGui import *
 
 RUNNING_IN_HELL = platform.system() == 'Windows'
 RUNNING_IN_STEVE_JOBS = platform.system() == 'Darwin'
 RUNNING_IN_GANOO_LOONIX = platform.system() == 'Linux'
+
+def Pixmap2StringIO(pixmap):
+
+    byteArray = QByteArray()
+    buffer = QBuffer(byteArray)
+
+    buffer.open(QIODevice.WriteOnly)
+    pixmap.save(buffer, "PNG")
+
+    strio = io.StringIO(byteArray)
+    strio.seek(0)
+
+    return strio
